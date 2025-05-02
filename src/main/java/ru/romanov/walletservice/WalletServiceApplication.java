76692d1a38ace8ba7ce216adceefe7775b814180
Romanov N.A. <kolya-2012@mail.ru>
2025-05-02 17:39:42 +0300

    Описана основная логика

--- src/main/java/ru/romanov/walletservice/WalletServiceApplication.java
+++ src/main/java/ru/romanov/walletservice/WalletServiceApplication.java
@@ -2,7 +2,7 @@ package ru.romanov.walletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
+
 
 @SpringBootApplication
 public class WalletServiceApplication {
--- src/main/java/ru/romanov/walletservice/config/RetryConfig.java
+++ src/main/java/ru/romanov/walletservice/config/RetryConfig.java
@@ -1,31 +1,9 @@
 package ru.romanov.walletservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 
 @EnableRetry
 @Configuration
 public class RetryConfig {
-
-//    @Bean
-//    public RetryTemplate retryTemplate() {
-//        RetryTemplate template = new RetryTemplate();
-//
-//        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
-//        backOffPolicy.setInitialInterval(50);
-//        backOffPolicy.setMultiplier(1.1);
-//        backOffPolicy.setMaxInterval(1000);
-//
-//        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
-//        retryPolicy.setMaxAttempts(2000);
-//
-//        template.setBackOffPolicy(backOffPolicy);
-//        template.setRetryPolicy(retryPolicy);
-//
-//        return template;
-//    }
 }
